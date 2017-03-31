4e6baefcf129cd79833da4b4c85b820aa14737db
smcochra <smcochra@gmail.com>
2017-03-31 15:54:36 -0700

    Initial Commit of TelnetAccessor and TelnetDriver
    Telnet Driver is low-level wrapper for telnetlib - should be called utilizing the class from TelnetAccessor, not directly...

---
+++ TelnetAccessor.py
@@ -0,0 +1,231 @@
+"""
+Created on Mar 23, 2017
+
+@author: smcochra
+
+3/27/2017 - edited by smcochra
+"""
+
+import TelnetDriver
+import datetime
+
+class TelnetAccessor(object):
+    """
+    Encompasses Send, Expect, SendExpect, Logging to DB with Timestamp
+    """
+    def __init__(self, debugFlag=False):
+        """
+        Instantiate TelnetDriver Class
+        + debugFlag - enable debug messaging
+        """
+        self.t = TelnetDriver.TelnetDriver(debugFlag=debugFlag)
+        
+    def open_console(self, console):
+        self.t.open(console)
+        
+    def close_console(self, console):
+        self.t.close(console)
+        
+    def send(self, data):
+        """
+        Sends data to open console
+        + data - string to push to socket
+        """
+        self.t.debug('Sending: %r' % data)
+        self.t.send(data)
+        
+    def expect_old(self, matchlist, timeout=5):
+        """
+        Arguments -
+        + matchlist - regex or list of regex's to match against
+        + timeout - seconds before timeout occurs
+        Returns tupple dictionary with following keys:
+        + 'matchidx' - index of item matched from argument matchlist
+        + 'matchobj' - MatchObject; see documentation re.MatchObject
+        + 'matchtext' - raw string that pattern matched against
+        + 'buffer' - buffer captured between start of expect call and timeout/match
+        + 'timeout_occured' - True or False
+        
+        """
+        idx, mobj, buf = self.t.expect(matchlist, timeout)
+        
+        returndict = {}
+        
+        timeout = True
+        mtext = None
+        if idx != -1:
+            timeout = False
+            # fetch entire match
+            mtext = mobj.group(0)
+        
+        returndict['matchidx'] = idx
+        returndict['matchobj'] = mobj
+        returndict['matchtext'] = mtext
+        returndict['buffer'] = buf
+        returndict['timeout_occured'] = timeout
+        
+        return returndict
+    
+    def expect(self, matchlist, timeout=5):
+        """
+        Arguments -
+        + matchlist - regex or list of regex's to match against
+        + timeout - seconds before timeout occurs
+        Returns tupple dictionary with following keys:
+        + 'matchidx' - index of item matched from argument matchlist
+        + 'matchobj' - MatchObject; see documentation re.MatchObject
+        + 'matchtext' - raw string that pattern matched against
+        + 'buffer' - buffer captured between start of expect call and timeout/match
+        + 'bufobj' - list of dictionaries ordered with key timestamp and value list
+                    of lines associated with timestamp
+                    [{time1: [buffer_line_1, buffer_line_2]},
+                     {time2: [buffer_line_1, buffer_line_2, ...],
+                     ...,
+                     ]
+        + 'timeout_occured' - True or False
+        + 'xtime' - execution time for expect to match
+        
+        """
+        returndict = {}
+        buffer = ''
+        
+        expobj = self.t.expect(matchlist, timeout)
+        
+        for dict in expobj['buffer']:
+            for timestamp, line in dict.iteritems():
+                buffer += '\n'.join(line)
+                buffer += '\n'
+        
+        # remove last new line that was applied in excess above
+        buffer = buffer[:-2]
+        
+        timeout = True
+        mtext = None
+        if expobj['midx'] != -1:
+            timeout = False
+            # fetch entire match
+            mtext = expobj['mobj'].group(0)
+    
+        returndict['matchidx'] = expobj['midx']
+        returndict['matchobj'] = expobj['mobj']
+        returndict['matchtext'] = mtext
+        returndict['buffer'] = buffer
+        returndict['bufobj'] = expobj['buffer']
+        returndict['xtime'] = expobj['xtime']
+        returndict['timeout_occured'] = timeout
+        
+        return returndict
+    
+    def sendexpect(self, data, matchlist, timeout=5, debug=False):
+        """
+        Combined send, expect;
+        """
+        self.send(data)
+        result = self.expect(matchlist, timeout=timeout)
+        
+        if debug:
+            self.__debug_expect(result)
+            
+        return result
+    
+    def sendexpect_list(self, data_list, matchlist, timeout=5, debug=False):
+        """
+        accepts list of commands to execute assuming same matchlist;
+        returns True after successful execution
+        + data_list - list of commands to run - '\r' will be appended to each command
+        + matchlist - regex or list of regex's to match against
+        + timeout - seconds before timeout occurs
+        + debug - print out expect return values 
+        """
+        returnobj = []
+        for data in data_list:
+            result = self.sendexpect(data + '\r', matchlist, timeout, debug)                
+            returnobj.append(result)
+            
+        return returnobj
+            
+            
+    def __debug_expect(self, exp_retrn_dict):
+        """
+        Print out expect return values
+        """        
+        self.t.set_debug_flag(True)
+        self.t.debug('------------------------------')
+        for key, value in exp_retrn_dict.iteritems():
+            self.t.debug('%s: %r' % (key, value))
+        self.t.debug('------------------------------')
+        
+    def print_log_with_timestamps(self, expect_obj_list):
+        """
+        Takes list of expect return objects and prints log
+        with timestamps
+        """
+        lastline = ''
+        buflist = []
+        for result in expect_obj_list:
+            buflist.append(result['bufobj'])
+
+        for cmd_dict_list in buflist:
+            # connect cmd sequence lastline w/ firstline
+            cmd_dict_list[0].values()[0][0] = lastline + cmd_dict_list[0].values()[0][0]
+            # get lastline of cmd sequence to tie to first line of next sequence
+            lastline = cmd_dict_list[-1].values()[0].pop()
+    
+            for bufobj in cmd_dict_list:
+                for timestamp, buf_list in bufobj.iteritems():
+                    for idx in range(len(buf_list)):
+                        print timestamp + '\t%s' % buf_list[idx]
+        
+        # don't forget to print lastline that we are storing               
+        print timestamp + '\t%s' % lastline
+    
+def logmsg(msg):
+    """
+    Logs msg to TBD location
+    """
+    time = gettimestamp()
+    print '%s\tlogmsg: %s' % (time, msg)
+    
+    
+def usermsg(msg):
+    """
+    Logs msg to TBD location
+    """
+    time = gettimestamp()
+    print '%s\tusermsg: %s' % (time, msg)
+    
+    
+def gettimestamp():
+    return datetime.datetime.now()
+    
+    
+def test(console='10.31.248.147:3009'):
+    usermsg('Testing User Message!')
+    logmsg('Testing log Message!')
+    
+    # prints output of all expect return values
+    # to provide support for debugging
+    debugFlag = False
+     
+    session = TelnetAccessor(debugFlag=debugFlag)
+    session.open_console(console)
+    
+    logmsg('Testing regular sendexpect...')
+    
+    #result = session.sendexpect('exit\r', '4X', debug=debugFlag)
+    #result = session.sendexpect('en\r', '4X', debug=debugFlag)
+    #result = session.sendexpect('skip\r', '4X', debug=debugFlag)
+    #result = session.sendexpect('show media\r', '4X', timeout=5, debug=debugFlag)
+    
+    logmsg('Testing sendexpect_list...')
+    
+    data_list = ['exit', 'en', 'skip', 'show media']
+
+    results = session.sendexpect_list(data_list, ['not_a_match', 'Net.*4X'], timeout=15, debug=debugFlag)
+
+    session.print_log_with_timestamps(results)
+    
+    usermsg('Done!')
+    logmsg('Done!')
+    
+test()
---
+++ TelnetDriver.py
@@ -0,0 +1,159 @@
+"""
+Created on Mar 23, 2017
+
+@author: smcochra
+
+3/27/2017 - edited by smcochra 
+"""
+
+import telnetlib
+import time
+import datetime
+import re
+
+class TelnetDriver(object):
+    '''
+    Wrapper for telnetlib. Shouldn't be called directly, use TelnetAccessor
+    '''
+    _ip = None
+    _port = None
+
+    def __init__(self, console=None, debugFlag=False):
+        '''
+        Pass console, return telnet session
+        + console - (optional) format <ip>:<port> e.g. '192.168.1.1:3003'
+        + debugFlag - enables debug messaging
+        '''
+        self.debugFlag = debugFlag
+        self.debug('Hello (telnet) World')
+        if console:
+            self.open(console)
+            
+    def set_debug_flag(self, flag):
+        """
+        Enable/Disable debug messaging
+        + flag - True/False
+        """
+        self.debugFlag = flag
+        
+    def open(self, console):
+        self.t = telnetlib.Telnet()
+
+        self._ip = console.split(':')[0]
+        self._port = int(console.split(':')[1])
+        
+        self.debug("Attempting to open connection with IP: '%s' Port: '%s'" % (self._ip, self._port))
+        self.t.open(self._ip, self._port)
+        self.debug("Session opened!")
+        
+    def send(self, data):
+        """
+        + data - string to push to socket
+        """
+        self.t.write(data)
+        
+    def expect_old(self, matchlist, timeout=5):
+        """
+        Returns tupple (idx, mtext, buf, timeout)
+        + idx - index of matched expr in matchlist; -1 if timeout occurs
+        + mtext - matchobject; if match, mtext.group(idx) returns actual matched text, else returns None
+        + buf - buffer from time of expect starts to timeout/match
+        """
+        self.t.cookedq = ''
+        
+        if not isinstance(matchlist, list):
+            matchlist = [matchlist]
+        
+        idx, mtext, buf = self.t.expect(matchlist, timeout=timeout)
+            
+        return (idx, mtext, buf)
+    
+    def expect(self, matchlist, timeout=5):
+        """
+        Accepts:
+        + matchlist - list of values to match against
+        + timeout - seconds before function should return if match is not met
+        
+        Returns dictionary:
+        + 'buffer' - list of dictionaries in format:
+                [{time1: [buffer_line_1, buffer_line_2]},
+                 {time2: [buffer_line_1, buffer_line_2, ...],
+                 ...,
+                 ]
+        + 'xtime' - execution time
+        + 'midx' - index of item matched in argument matchlist; -1 if no match found
+        + 'mobj' - match object returned by re.search; None if no match found
+        """
+        if not isinstance(matchlist, list):
+            matchlist = list(matchlist)
+    
+        # define list of dictionaries;
+        # list insures order is perserved rather than
+        # sorting down the road...
+        running_buf = []
+        # remember last line of buffer to append to first line of next buf
+        last_line_buf = ''
+        
+        # matchidx initialized to 0
+        midx = -1
+        # index of timestamp we are iterating with
+        tidx = 0
+        
+        # compile regex's once to save processing
+        compiled_regex = []
+        for regex in matchlist:
+            compiled_regex.append(re.compile(regex))
+        
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            buf = self.t.read_very_eager()
+            if buf:
+                # create list of lines associated with buffer
+                buf = buf.replace('\r', '')
+                buf_list = buf.split('\n')
+                
+                # first line is really cut off part from last line
+                last_idx = len(buf_list) - 1
+                buf_list[0] = last_line_buf + buf_list[0]
+                last_line_buf = buf_list[last_idx]
+                
+                # buffer to remember
+                timestamp = str(datetime.datetime.now())
+                running_buf.append({timestamp: []})
+                # search each line in most recent buf for regex match
+                # don't search last line because it is incomplete,
+                # last line is appended to start of first line of next buf
+                for i in range(len(buf_list)):
+                    # don't append last item
+                    if i < last_idx:                     
+                        running_buf[tidx][timestamp].append(buf_list[i])
+                    
+                    # iterate thru matchlist to look for matches in this line
+                    for idx in range(len(compiled_regex)):
+                        # compute regex
+                        mobj = re.search(compiled_regex[idx], buf_list[i])
+                        if mobj:
+                            # don't forget to append last line we were saving
+                            running_buf[tidx][timestamp].append(last_line_buf)
+                            return {'buffer': running_buf,
+                                    'xtime': time.time() - start_time,
+                                    'midx': idx,
+                                    'mobj': mobj} 
+                        
+                tidx += 1
+
+        xtime = time.time() - start_time
+        return {'buffer': running_buf, 'xtime': xtime, 'midx': midx, 'mobj': mobj}     
+        
+    def close(self):
+        """
+        Close opened session
+        """
+        self.t.close()
+
+    def debug(self, msg):
+        """
+        Dump debug info - can be changed easily from here...
+        """
+        if self.debugFlag:
+            print msg
